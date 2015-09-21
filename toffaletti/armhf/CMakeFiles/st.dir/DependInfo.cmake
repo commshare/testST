3d7af3661c608e54a23746ee24e6233d2d79f5ba
commshare <commshare.zhang@gmail.com>
2015-09-21 12:08:24 +0800

    toff clean from pc

---
+++ toffaletti/armhf/CMakeFiles/st.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/event.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/event.c.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/io.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/io.c.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/key.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/key.c.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/md.S" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/md.S.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/sched.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/sched.c.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/stk.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/stk.c.o"
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/sync.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/sync.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  "MD_HAVE_EPOLL"
+  "NVALGRIND"
+  "_GNU_SOURCE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ toffaletti/armhf/CMakeFiles/threaded.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/examples/threaded.c" "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/threaded.dir/examples/threaded.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  "MD_HAVE_EPOLL"
+  "NVALGRIND"
+  "_GNU_SOURCE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zhangbin/nuf/nfshome/st/toffaletti/armhf/CMakeFiles/st.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
